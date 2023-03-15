93e3e0a8f8c06e12309c1b2ef97828484e6f9daf
Hatem <Hatemganash95@gmai.com>
2023-03-15 09:41:39 -0700

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Hatem on 15/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
